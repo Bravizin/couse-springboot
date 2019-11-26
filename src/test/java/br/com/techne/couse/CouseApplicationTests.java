9cb94d2d724bbcb12df84d1e0e2bb787e184f432
Leonardo Selmes <leo_rse@hotmail.com>
2019-11-26 14:00:09 -0300

    Project Created

---
+++ src/test/java/br/com/techne/couse/CouseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techne.couse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
